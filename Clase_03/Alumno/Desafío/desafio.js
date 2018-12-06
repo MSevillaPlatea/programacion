a54252ce70f59b5ee121dbcfbfb8746611a18260
MSevillaPlatea <msevilla@plateanet.com>
2018-12-06 12:24:38 -0300

    desafio de operacion completado

--- Clase_03/Alumno/Desafío/desafio.js
+++ Clase_03/Alumno/Desafío/desafio.js
@@ -1,10 +1,39 @@
 var i = 1;
-
+var ressultado;
 do {
-    var operacion = prompt("ingresar que desea hacer: suma, resta, division o multiplicacion");
+    var operacion = prompt("que desea hacer: suma, resta, division o multiplicacion");
     i++
-} while (operacion != "suma")
+} while (operacion !== "suma" && operacion !== "resta" && operacion !== "division" && operacion !== "multiplicacion")
 console.log(operacion);
 
-var num_uno = prompt("ingresar el primer numero");
-var num_dos = prompt("ingresar el segundo numero");
+do {
+    var num_uno = prompt("ingresar el primer numero");
+} while (parseInt(num_uno) % 1 !== 0);
+
+console.log(num_uno);
+
+do {
+    var num_dos = prompt("ingresar el segundo numero");
+} while (parseInt(num_dos) % 1 !== 0);
+
+console.log(num_dos);
+
+
+switch (operacion) {
+    case "suma":
+        ressultado = parseInt(num_uno) + parseInt(num_dos);
+        break;
+    case "resta":
+        ressultado = parseInt(num_uno) - parseInt(num_dos);
+        break;
+    case "division":
+        ressultado = parseInt(num_uno) / parseInt(num_dos);
+        break;
+    case "multiplicacion":
+        ressultado = parseInt(num_uno) * parseInt(num_dos);
+        break;
+    default:
+        break;
+}
+
+alert(ressultado);
