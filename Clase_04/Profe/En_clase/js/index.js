c881c51238d73b4049f229abfb4e2353b1ee96b6
Ana Turrillo <anaturrillo@gmail.com>
2018-12-11 21:40:47 -0300

    clase 5

--- Clase_04/Profe/En_clase/js/index.js
+++ Clase_04/Profe/En_clase/js/index.js
@@ -1,1 +1,34 @@
-console.log('Init app clase 04')
+var notas = [7, 6, 10, 2, 3, 5, 9];
+
+var avCalculator = function(notasAlumno){
+  var suma = 0;
+
+  for(var i = 0; i < notasAlumno.length; i++){
+    var nota = notasAlumno[i];
+    suma = suma + nota;
+  }
+
+  return suma / notasAlumno.length;
+};
+
+avCalculator(notas);
+
+var palabraMasLarga = function (frase) {
+  var palabras = frase.split(' ');
+  var ganadora = '';
+
+  for(var i = 0; i < palabras.length; i++){
+    var palabra = palabras[i];
+    if (ganadora.length < palabra.length) {
+      ganadora = palabra;
+    }
+  }
+  return ganadora;
+};
+
+var buscador = function(texto, alumnos){
+
+};
+
+
+
