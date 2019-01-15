a2b875a37d37b636584cc295a6109cf9054ed85a
Ana Turrillo <anaturrillo@gmail.com>
2019-01-15 20:24:24 -0300

    archivos top secret de los profes

--- Clase_14/Profe/En_clase/Efectos/js/index.js
+++ Clase_14/Profe/En_clase/Efectos/js/index.js
@@ -1,9 +1,37 @@
 $(document).ready(function () {
 
+  var counter = 0;
+  var setCounter = function (counter) {
+    $('#counter').text(counter);
+
+    return counter + 1
+  };
+
+  setCounter(counter);
+
   $(document).keydown(function (event) {
-    console.log(event.which)
+
+    var key = event.which;
+
+    if (key === 38) { //arriba
+      counter = counter + 1;
+    }
+
+    if (key === 40) { // abajo
+      counter = counter - 1
+    }
+
+    if (key === 13) { // enter
+      counter = 0
+    }
+
+    setCounter(counter)
+
   });
 
 
+  $('#miId').click(function () {
+
+  })
 
 });
