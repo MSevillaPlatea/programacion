d7f668fdc1ff165383609e695fdbf087acea1e02
MSevillaPlatea <msevilla@plateanet.com>
2019-01-20 19:46:46 -0300

    desafio 8 completado

--- Clase_09/Alumno/en_clase/clase9.js
+++ Clase_09/Alumno/en_clase/clase9.js
@@ -41,47 +41,69 @@ boton.onclick = function() {
 // ──────────────────────────────────────────────────────────────────────────────────────────
 //
 
-var boton = document.getElementById("boton");
+/* var boton = document.getElementById("boton");
 
 var box = document.getElementById("caja");
 
 
-//console.log(box.indexOf("box"));
+console.log(box.indexOf("box"));
 
 boton.onclick = function() {
 
-    box.classList.toggle("otro");
+    box.classList.toggle("otro"); */
 
 
-    // var esnegro = box.indexOf("otro") !== -1;
+// var esnegro = box.indexOf("otro") !== -1;
 
-    // console.log(box.indexOf("otro"));
+// console.log(box.indexOf("otro"));
 
-    /* if (esnegro) {
-        box.classList.add("otro");
-        box.classList.remove("box");
+/* if (esnegro) {
+    box.classList.add("otro");
+    box.classList.remove("box");
+} else {
+    box.classList.add("box");
+    box.classList.remove("otro");
+} 
+
+}*/
+
+
+
+//
+// ────────────────────────────────────────────────────────────────── I ──────────
+//   :::::: V A L I D A R   E M A I L : :  :   :    :     :        :          :
+// ────────────────────────────────────────────────────────────────────────────
+//
+
+var campo_email = $("#correo");
+
+console.log(campo_email.val());
+
+
+function validar(event) {
+    elemento = $(event.target);
+    console.log(elemento.val());
+
+    if (!elemento.val()) {
+        console.log("campo vacio");
+    } else if (elemento.val().indexOf('@') === -1) {
+        console.log("no es correo valido");
+        elemento.removeClass('is-valid');
+        elemento.addClass('is-invalid');
+    } else if (elemento.val().indexOf('.') === -1) {
+        console.log("no es correo valido");
+        elemento.removeClass('is-valid');
+        elemento.addClass('is-invalid');
     } else {
-        box.classList.add("box");
-        box.classList.remove("otro");
-    } */
+        $("body").html(elemento.val());
+        console.log("correo valido");
+        elemento.addClass('is-valid');
+    }
 
 }
 
 
 
-//
-// ────────────────────────────────────────────────────────────────────────────── I ──────────
-//   :::::: V A L I D A R   C A M P O   V A C I O : :  :   :    :     :        :          :
-// ────────────────────────────────────────────────────────────────────────────────────────
-//
-
-var campo = document.getElementById("campo");
 
 
-
-campo.onblur = function() {
-
-
-
-
-}
+campo_email.blur(validar);
