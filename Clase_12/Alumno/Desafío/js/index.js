43e311fd312f175fadbd93087deec6f628080603
MSevillaPlatea <msevilla@plateanet.com>
2019-01-21 22:01:08 -0300

    desafio 10 completado

---
+++ Clase_12/Alumno/Desafío/js/index.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+
+
+
+    $("#email").blur(function(e) {
+        var elemento = $(e.target);
+
+
+        if (!elemento.val()) {
+            console.log("campo vacio");
+        } else if (elemento.val().indexOf('@') === -1) {
+            $("#indicador").html("<p>correo invalido</p>");
+            console.log("no es correo valido");
+            elemento.removeClass('is-valid');
+            elemento.addClass('is-invalid');
+        } else if (elemento.val().indexOf('.') === -1) {
+            $("#indicador").html("<p>correo invalido</p>");
+            console.log("no es correo valido");
+            elemento.removeClass('is-valid');
+            elemento.addClass('is-invalid');
+        } else {
+            $("#indicador").html("<p>correo valido</p>");
+            console.log("correo valido");
+            elemento.removeClass('is-invalid');
+            elemento.addClass('is-valid');
+        }
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+/* if (!elemento.val()) {
+    console.log("campo vacio");
+} else if (elemento.val().indexOf('@') === -1) {
+    console.log("no es correo valido");
+    elemento.removeClass('is-valid');
+    elemento.addClass('is-invalid');
+} else if (elemento.val().indexOf('.') === -1) {
+    console.log("no es correo valido");
+    elemento.removeClass('is-valid');
+    elemento.addClass('is-invalid');
+} else {
+    $("body").html(elemento.val());
+    console.log("correo valido");
+    elemento.addClass('is-valid');
+} */
