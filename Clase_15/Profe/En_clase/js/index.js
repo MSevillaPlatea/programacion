6eb1fe5fde0e607337fd62ba9e455507bc2572a4
Ana Turrillo <anaturrillo@gmail.com>
2019-01-31 18:56:20 -0300

    todas las clases subidas

--- Clase_15/Profe/En_clase/js/index.js
+++ Clase_15/Profe/En_clase/js/index.js
@@ -1,4 +1,35 @@
 $(document).ready(function(){
+
+  /*var mostrarDatos = function (results) {
+    console.log('muestro datos', results);
+
+    var tabla = $('#list');
+
+    for (var i = 0; i < results.length; i++){
+      console.log(results[i].name)
+      tabla.append('<tr>' +
+        '<td>'+ results[i].name+ '</td>' +
+        '<td>'+ results[i].height+ '</td>' +
+        '<td>'+ results[i].gender+ '</td>' +
+        '<td>'+ results[i].hair_color+ '</td>' +
+        '</tr>')
+    }
+
+  };
+
+  console.log('empieza la request');
+
+  $.ajax('https://swapi.co/api/people')
+    .done(function (data) {
+      console.log('termina la req', data);
+      mostrarDatos(data.results)
+
+    })
+    .fail(function (error) {
+      console.log('error', error.status)
+    });
+
+*/
   var misDatos = [];
   var showPeople = function (event) {
     var uri = $(event.target).parent().attr('data');
@@ -24,15 +55,16 @@ $(document).ready(function(){
 
   var displayPersonInfo = function (data) {
     document.createElement('ul');
-
+    console.log(data)
     $.ajax(data.homeworld)
       .done(function(homeworld){
+        console.log(homeworld)
         $('#caracter_info').html('<h2>'+ data.name +'</h2>' +
           '<ul>' +
           '<li>Altura: ' + data.height + '</li>' +
           '<li>Género: ' + data.gender + '</li>' +
           '<li>Color de pelo: ' + data.hair_color + '</li>' +
-          '<li>Planeta de origen: ' + homeworld.name + '</li>' +
+          '<li>Planeta de origen: ' + homeworld.  name + '</li>' +
           '</ul>')
       })
 
@@ -41,6 +73,7 @@ $(document).ready(function(){
 
   $.ajax('https://swapi.co/api/people/')
     .done(function(data){
+
       misDatos = data.results;
       console.log('me devolvio la llamada')
       var people = data.results;
@@ -61,6 +94,42 @@ $(document).ready(function(){
     console.log('datos del servidor', misDatos)
 
   }, 2000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
 
 
+/**
+ *
+ *
+ *
+
+ */
