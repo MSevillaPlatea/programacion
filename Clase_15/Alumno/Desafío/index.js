8bced567f426810085edaddc98d01fdba905f85e
MSevillaPlatea <msevilla@plateanet.com>
2019-02-03 20:11:07 -0300

    desafio_13 actualizado

--- Clase_15/Alumno/Desafío/index.js
+++ Clase_15/Alumno/Desafío/index.js
@@ -72,7 +72,7 @@ $(document).ready(function() {
             .done(function(data) {
                 console.log("se cargaron los personajes")
                 for (var i = 0; i < data.results.length; i++) {
-                    $("#tableBody").append("<tr><th>" + i + "</th>" + "<th>" + data.results[i].name + "</th>" + "<th>" + data.results[i].gender + "</th>" + "<th>" + data.results[i].height + "</th>" + "<th>" + data.results[i].mass + "</th>" + "<th>" + data.results[i].eye_color + "</th>" + "<th>" + "<button class='btn btn-danger'>Guardar</button>" + "</th></tr>");
+                    $("#tableBody").append("<tr><th>" + i + "</th>" + "<th>" + data.results[i].name + "</th>" + "<th>" + data.results[i].gender + "</th>" + "<th>" + data.results[i].height + "</th>" + "<th>" + data.results[i].mass + "</th>" + "<th>" + data.results[i].eye_color + "</th>" + "<th>" + "</th></tr>");
                     console.log(data.results[i].name);
                 }
             })
